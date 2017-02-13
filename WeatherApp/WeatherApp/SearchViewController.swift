73800f03394eebe1fbfd3af1a3e964187b3bfaca
Nguyen Hoang Tuan <hoangtuanfithou@gmail.com>
2017-02-13 14:38:45 +0700

    rename to SearchViewController

--- WeatherApp/WeatherApp/SearchViewController.swift
+++ WeatherApp/WeatherApp/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  WeatherApp
 //
 //  Created by Nguyen Hoang Tuan on 2/8/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     var histories: [WeatherResponse] = []
     
